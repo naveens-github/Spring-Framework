0c42704d6f4b8d190e3533c982803426e78b60a6
naveen <naveenwork8@gmail.com>
2023-04-16 11:37:20 -0500

    scopes

--- SpringFrameworkDemo/src/main/java/com/myproject/Scopes/BeanConfig.java
+++ SpringFrameworkDemo/src/main/java/com/myproject/Scopes/BeanConfig.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com.myproject.Scopes")//Just this annotation is enough to load the beans
 public class BeanConfig {
     //we can also load the beans without @ComponentScan as follows. We can remove @Component from Doctor class as well
-    @Bean
-    public Doctor doctor(){
-        return new Doctor();
-    }
+//    @Bean
+//    public Doctor doctor(){
+//        return new Doctor();
+//    }
 }
--- SpringFrameworkDemo/src/main/java/com/myproject/Scopes/Doctor.java
+++ SpringFrameworkDemo/src/main/java/com/myproject/Scopes/Doctor.java
@@ -1,7 +1,12 @@
 package com.myproject.Scopes;
 
 import com.myproject.AnnotationBasedConfiguration.Staff;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+//@Scope(scopeName = "singleton")
+@Component
+@Scope(scopeName = "prototype") // if we want to use Doctor object multiple times, container will gives a new object.
 public class Doctor implements Staff {
     private String qualification;
 
